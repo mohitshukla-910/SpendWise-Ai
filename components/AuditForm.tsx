6de9c24360c34cd23ddb315f026d1a43a354c0a4
mohit shukla <mohitshukla9102@gmail.com>
2026-05-11 07:10:46 +0530

    feat: add basic audit logic engine

--- components/AuditForm.tsx
+++ components/AuditForm.tsx
@@ -1,4 +1,35 @@
+"use client";
+import { useState } from "react";
 export default function AuditForm() {
+  const [tool, setTool] = useState("ChatGPT");
+  const [monthlySpend, setMonthlySpend] = useState("");
+  const [result, setResult] = useState("");
+  const [savings, setSavings] = useState(0);
+ const generateAudit = () => {
+
+  if (tool === "ChatGPT" && Number(monthlySpend) > 100) {
+
+    setResult(
+      
+      "Your ChatGPT spending appears high for a small team. You may save money by switching to a lower-tier plan."
+    );
+     setSavings(20);
+  } else if (tool === "Cursor" && Number(monthlySpend) > 50) {
+
+    setResult(
+      "Cursor Business may be unnecessary unless your team heavily relies on collaborative AI coding workflows."
+    );
+    setSavings(15);
+
+  } else {
+
+    setResult(
+      "Your AI spending looks reasonable based on the provided inputs."
+    );
+   setSavings(0)
+  }
+
+};
   return (
     <div className="mt-20 bg-zinc-900 p-8 rounded-2xl border border-zinc-800 max-w-3xl mx-auto">
 
@@ -12,8 +43,11 @@ export default function AuditForm() {
           <label className="block mb-2 text-sm text-gray-400">
             AI Tool
           </label>
-
-          <select className="w-full bg-black border border-zinc-700 rounded-xl px-4 py-3">
+  <select
+  value={tool}
+  onChange={(e) => setTool(e.target.value)}
+  className="w-full bg-black border border-zinc-700 rounded-xl px-4 py-3"
+>
             <option>ChatGPT</option>
             <option>Claude</option>
             <option>Cursor</option>
@@ -42,7 +76,10 @@ export default function AuditForm() {
           <input
             type="number"
             placeholder="100"
-            className="w-full bg-black border border-zinc-700 rounded-xl px-4 py-3"
+           value={monthlySpend}
+           onChange={(e) => setMonthlySpend(e.target.value)}
+             className="w-full bg-black border border-zinc-700 rounded-xl px-4 py-3"
+
           />
         </div>
 
@@ -85,9 +122,26 @@ export default function AuditForm() {
 </div>
       </div>
 
-      <button className="mt-8 w-full bg-white text-black py-3 rounded-xl font-semibold hover:bg-gray-200 transition">
+      <button 
+      onClick={generateAudit}
+      className="mt-8 w-full bg-white text-black py-3 rounded-xl font-semibold hover:bg-gray-200 transition">
         Generate Audit
       </button>
+    {result && (
+  <div>
+
+    <div className="mt-6 bg-black border border-zinc-700 rounded-xl p-4 text-center">
+      {result}
+    </div>
+
+    {savings > 0 && (
+      <div className="mt-4 text-green-400 text-center font-semibold">
+        Potential Savings: ${savings}/month
+      </div>
+    )}
+
+  </div>
+)}
 
     </div>
   );
